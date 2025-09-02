ce245d4e11836a02db14a0c49df53f32d0ef8fb2
Matias Bordon <matzaia2001@gmail.com>
2025-09-02 19:19:47 -0300

    crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'blog'
 
 urlpatterns = [
-
-  path ('', views.post_list, name= 'post_list')
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/list/', views.PostListView.as_view(), name='post_list'),
+    path('post/detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 from .models import Post
 
-def post_list(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': post_list})
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get('busqueda')
+        if busqueda:
+            queryset = queryset.filter(titulo__icontains=busqueda)
+        return queryset
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+    template_name = 'blog/post_confirm_delete.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['titulo', 'contenido']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['titulo', 'contenido']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
